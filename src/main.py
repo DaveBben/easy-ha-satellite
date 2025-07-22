ad4e6c33acf92724c27b18a12684eb2d0709d9fc
David bennett <david.a.bennett7@gmail.com>
2025-07-21 22:05:39 -0400

    Reworked process to use Home Assistant STT

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
+"""
+This program acts as a home assistant satellite. It runs a local wakeword model. When
+the wakeword is detected, it will forward audio to Home Assistant for Speech to text
+and to handle intents.
+"""
+
 import asyncio
 import json
 import logging
 import multiprocessing as mp
 import os
 import queue
-import time
+from multiprocessing.synchronize import Event
 from pathlib import Path
 from typing import Any
 
@@ -18,7 +24,9 @@ from openwakeword.model import Model
 from websockets.exceptions import ConnectionClosed
 
 # Environment Variables
-BASE_URL = os.environ.get("BASE_URL", "192.168.1.16:9292")
+HASS_TOKEN = os.environ.get("HASS_TOKEN")
+BASE_URL = os.environ.get("BASE_URL")
+SECURE_WEBSOCKET = os.environ.get("SECURE_WEBSOCKET", "true")
 INPUT_DEVICE_NAME = os.environ.get("INPUT_AUDIO_DEVICE", None)
 OUTPUT_DEVICE_NAME = os.environ.get("OUTPUT_AUDIO_DEVICE", None)
 WAKEWORD_MODEL = os.environ.get("WAKEWORD_MODEL_NAME", "hey_jarvis")
@@ -39,19 +47,22 @@ PROJECT_ROOT = Path(__file__).resolve().parent
 SOUND_DIR = PROJECT_ROOT / "sounds"
 AUDIO_CLIPS = {
     "listen_start": sf.read(SOUND_DIR / "listen_start.wav", dtype="int16"),
-    "listen_end": sf.read(SOUND_DIR / "listen_end.wav", dtype="int16"),
+    "task_complete": sf.read(SOUND_DIR / "task_complete.wav", dtype="int16"),
     "connected": sf.read(SOUND_DIR / "connected.wav", dtype="int16"),
     "error": sf.read(SOUND_DIR / "failed.wav", dtype="int16"),
 }
 
 # Server
-WS_SERVER_URL = f"ws://{BASE_URL}/stream"
-RECONNECT_DELAY_SECONDS = 5  # Time to wait before trying to reconnect\
+WS_SERVER_URL = f"{BASE_URL}/api/websocket"
+RECONNECT_DELAY_SECONDS = 5  # Time to wait before trying to reconnect
 WS_CONFIG = {"ping_interval": 20, "ping_timeout": 20}
+PIPELINE_ID = 1
+PIPELINE_TIMEOUT_SECS = 60
 
 # WakeWord
 REQUIRED_WAKEWORD_MS = 80
 COOLDOWN_MS = 3000
+WAKEWORD_EVENT = "WAKEWORD_DETECTED"
 
 
 _output_stream = sd.OutputStream(
@@ -76,10 +87,9 @@ def wakeword_process_worker(
     event_q: mp.Queue,
     model_name: str,
     inference_framework: str,
-    shutdown_event: Any,
-    streaming_event: Any,
+    shutdown_event: Event,
+    wake_word_run_event: Event,
 ):
-    logging.info("Wakeword process started.")
     try:
         oww_model = Model(
             wakeword_models=[model_name], inference_framework=inference_framework
@@ -87,9 +97,11 @@ def wakeword_process_worker(
     except Exception:
         logging.critical("Failed to load model in subprocess.", exc_info=True)
         return
+    logging.info(f"Listening for {model_name}...")
     while not shutdown_event.is_set():
         try:
-            if not streaming_event.is_set():
+            wake_word_run_event.wait()
+            while wake_word_run_event.is_set():
                 chunk = audio_q.get(timeout=AUDIO_CONFIG["chunk_ms"] / 1000)
                 if chunk is None:  # Sentinel value to stop the process
                     break
@@ -99,10 +111,8 @@ def wakeword_process_worker(
                     logger.info(
                         f"Wakeword detected! (Score: {prediction[model_name]:.2f})"
                     )
-                    event_q.put("WAKEWORD_DETECTED")
+                    event_q.put(WAKEWORD_EVENT)
                     oww_model.reset()
-            else:
-                time.sleep(1)
 
         except queue.Empty:
             # This is expected when no audio is coming in, just continue
@@ -110,45 +120,109 @@ def wakeword_process_worker(
         except KeyboardInterrupt:
             break
         except Exception:
-            logger.error("Error in wakeword process", exc_info=True)
+            logger.error("Error in Keyword detection process", exc_info=True)
 
 
 def play_sound(sound_data: tuple[np.ndarray[Any, np.dtype[Any]]]):
-    """A synchronous function that plays a sound file (this will block)."""
     try:
-        # sd.play(sound_data[0], sound_data[1], blocking=False)
         _output_stream.write(sound_data[0])
     except Exception:
         logger.error("Could not play sound")
 
 
-async def handle_server_messages(websocket: websockets.ClientConnection) -> None:
+async def handle_server_messages(
+    websocket: websockets.ClientConnection, stt_active: asyncio.Event
+) -> None:
     """Handle incoming messages from the server and play sounds."""
     logger.info("Message handler task started.")
     try:
         async for message in websocket:
             try:
                 data = json.loads(message)
-                event_type = data.get("event")
+                match data:
+                    case {"type": "auth_required"}:
+                        await websocket.send(
+                            json.dumps(
+                                {
+                                    "type": "auth",
+                                    "access_token": HASS_TOKEN,
+                                }
+                            )
+                        )
+                    case {"type": "auth_ok"}:
+                        logger.info("Successfully authenticated with Home Assistant")
+                        await websocket.send(
+                            json.dumps(
+                                {
+                                    "id": PIPELINE_ID,
+                                    "type": "assist_pipeline/run",
+                                    "start_stage": "stt",
+                                    "timeout": PIPELINE_TIMEOUT_SECS,
+                                    "end_stage": "intent",
+                                    "input": {
+                                        "sample_rate": AUDIO_CONFIG["sample_rate"]
+                                    },
+                                }
+                            )
+                        )
+                    # Speech to text started
+                    case {"event": {"type": "stt-start"}}:
+                        play_sound(AUDIO_CLIPS["listen_start"])
+                        stt_active.set()
+                    # Speech to Text Complete
+                    case {"event": {"type": "stt-end"}}:
+                        try:
+                            phrase = data["event"]["data"]["stt_output"]["text"]
+                            logger.info(f"Command: {phrase}")
+                        except KeyError:
+                            logger.warning("STT complete, but no phrase transcribed")
+                        stt_active.clear()
+                    # Error
+                    case {"event": {"type": "error"}}:
+                        try:
+                            err_msg = data["event"]["data"]["message"]
+                            logger.warning(err_msg)
+                        except KeyError:
+                            logger.warning(
+                                "Pipeline Error but could not retrieve reponse"
+                            )
+                        stt_active.clear()
+                        play_sound(AUDIO_CLIPS["error"])
 
-                # TODO: Use Structured Events and Match Case
-                if event_type == "listen_started":
-                    logger.info("Server started listening")
-                    play_sound(AUDIO_CLIPS["listen_start"])
-                elif event_type == "listen_stopped":
-                    logger.info("Server stopped listening")
-                    play_sound(AUDIO_CLIPS["listen_end"])
-                elif event_type == "action_failed":
-                    logger.info("Server Failed to complete action")
-                    # TODO: Won't play because listen_stopped plays first
-                    play_sound(AUDIO_CLIPS["error"])
-                else:
-                    logger.info(f"Server message: {data}")
+                    # Action Successful
+                    case {
+                        "event": {
+                            "data": {
+                                "intent_output": {
+                                    "response": {"response_type": "action_done"}
+                                }
+                            }
+                        }
+                    }:
+                        play_sound(AUDIO_CLIPS["task_complete"])
+                        break
+
+                    # Action Failed
+                    case {
+                        "event": {
+                            "data": {
+                                "intent_output": {
+                                    "response": {"response_type": "error"}
+                                }
+                            }
+                        }
+                    }:
+                        play_sound(AUDIO_CLIPS["error"])
+                        break
+
+                    case _:
+                        logger.debug(f"Server message: {data}")
 
             except json.JSONDecodeError:
                 logger.info(f"Received non-JSON server response: {message}")
             except Exception as e:
                 logger.error(f"Error processing server message: {e}")
+                break
 
     except ConnectionClosed:
         logger.info("Connection closed")
@@ -160,56 +234,82 @@ async def handle_server_messages(websocket: websockets.ClientConnection) -> None
         raise
 
 
-async def stream_audio(websocket: websockets.ClientConnection, audio_q: mp.Queue):
+async def stream_audio(
+    websocket: websockets.ClientConnection,
+    audio_q: mp.Queue,
+    stt_active: asyncio.Event,
+):
     loop = asyncio.get_running_loop()
     try:
-        while True:
+        await stt_active.wait()
+        logger.info("🎤 Microphone stream is now active.")
+        while stt_active.is_set():
             chunk = await loop.run_in_executor(None, audio_q.get)
             if chunk is None:
                 break
-            await websocket.send(chunk)
+            # Need to send handler ID as part of audio
+            handler_id = bytes([PIPELINE_ID])
+            await websocket.send(handler_id + chunk)
     except ConnectionClosed:
         logger.info("Stopped streaming audio to server.")
+    except asyncio.CancelledError:
+        logger.info("stream audio tasks stopped")
     except Exception:
         logger.exception("Message handler encountered a fatal error.")
         raise
+    finally:
+        logger.info("🛑 Microphone stream is now stopped")
 
 
 async def listen_for_wakeword(
     audio_queue: mp.Queue,
     event_queue: mp.Queue,
-    shutdown_event: Any,
-    streaming_event: Any,
+    shutdown_event: Event,
+    wake_word_run_event: Event,
 ) -> None:
     """Waits for a wakeword detection event and then manages the server interaction."""
     loop = asyncio.get_running_loop()
+    stt_active = asyncio.Event()
+    wake_word_run_event.set()
+
+    # Create Server URL
+    server_url = f"wss://{WS_SERVER_URL}"
+    if str(SECURE_WEBSOCKET.lower()) == "false":
+        server_url = f"ws://{WS_SERVER_URL}"
+    logger.info(f"Using HomeAssistant URl {server_url}")
+
+    # Main Loop
     while not shutdown_event.is_set():
         event = await loop.run_in_executor(None, event_queue.get)
         if event is None:
             break
-
-        if event == "WAKEWORD_DETECTED":
-            streaming_event.set()
+        if event == WAKEWORD_EVENT:
+            wake_word_run_event.clear()
             try:
                 # When wakeword is detected, connect to WebSocket and stream
-                async with websockets.connect(WS_SERVER_URL, **WS_CONFIG) as websocket:
-                    logger.info("✅ WebSocket connection established.")
-                    logger.info("🎤 Microphone stream is now active.")
-
+                async with websockets.connect(server_url, **WS_CONFIG) as websocket:
                     try:
+                        logger.info("✅ WebSocket connection established.")
                         async with asyncio.TaskGroup() as tg:
-                            tg.create_task(handle_server_messages(websocket))
-                            tg.create_task(stream_audio(websocket, audio_queue))
+                            tg.create_task(
+                                handle_server_messages(websocket, stt_active)
+                            )
+                            tg.create_task(
+                                stream_audio(websocket, audio_queue, stt_active)
+                            )
                     except* Exception as eg:
                         for exc in eg.exceptions:
                             if not isinstance(
-                                exc, (ConnectionClosed, asyncio.CancelledError)
+                                exc,
+                                (
+                                    ConnectionClosed,
+                                    asyncio.CancelledError,
+                                    TimeoutError,
+                                ),
                             ):
                                 logger.error(
                                     f"Unhandled exception in task group: {exc!r}"
                                 )
-                    finally:
-                        play_sound(AUDIO_CLIPS["listen_end"])
 
             except ConnectionRefusedError:
                 logger.warning("Server is offline. Could not connect.")
@@ -217,12 +317,15 @@ async def listen_for_wakeword(
                 await asyncio.sleep(RECONNECT_DELAY_SECONDS)
             except Exception:
                 logger.exception("An error occurred during WebSocket communication.")
+                play_sound(AUDIO_CLIPS["error"])
             finally:
-                streaming_event.clear()
+                while not audio_queue.empty():
+                    audio_queue.get_nowait()
+                wake_word_run_event.set()
 
 
 async def run_client(
-    audio_queue: mp.Queue, event_queue: mp.Queue, shutdown_event, streaming_event
+    audio_queue: mp.Queue, event_queue: mp.Queue, shutdown_event, wake_word_run_event
 ) -> None:
     """Main client function with a persistent reconnection loop."""
 
@@ -248,7 +351,7 @@ async def run_client(
                 ),
             ):
                 await listen_for_wakeword(
-                    audio_queue, event_queue, shutdown_event, streaming_event
+                    audio_queue, event_queue, shutdown_event, wake_word_run_event
                 )
 
         except sd.PortAudioError as e:
@@ -263,7 +366,10 @@ async def run_client(
 
 
 def main() -> None:
-    logger.info("Starting WebSocket audio client...")
+    if not HASS_TOKEN:
+        raise ValueError("Home Assistant Long Lived token is not set!")
+    if not BASE_URL:
+        raise ValueError("Home Assistant Base URL is not set!")
     try:
         print("\n" + "-" * 50)
         print("Available audio input devices:")
@@ -289,7 +395,7 @@ def main() -> None:
         # Shutdown tasks and threads
         shutdown_event = mp.Event()
         # Signal whether we are streaming
-        streaming_event = mp.Event()
+        wake_word_run_event = mp.Event()
 
         # Start Dedicated Wakeword Process
         wakeword_process = mp.Process(
@@ -300,7 +406,7 @@ def main() -> None:
                 WAKEWORD_MODEL,
                 INFERENCE_FRAMEWORK,
                 shutdown_event,
-                streaming_event,
+                wake_word_run_event,
             ),
             daemon=True,  # auto-terminate with the main process
         )
@@ -309,7 +415,7 @@ def main() -> None:
         loop = uvloop.new_event_loop()
         asyncio.set_event_loop(loop)
         main_task = loop.create_task(
-            run_client(audio_queue, event_queue, shutdown_event, streaming_event)
+            run_client(audio_queue, event_queue, shutdown_event, wake_word_run_event)
         )
 
         try:
---
+++ todo.md
@@ -0,0 +1,4 @@
+[7/21/ 9:10 AM]
+* Got auth working but Home Assistant returning error
+* Need to send handler_id for every binary message
+* Refer to [this](https://github.com/slackr31337/PorcupinePipeline/blob/main/voice_pipeline.py#L423) implementation
